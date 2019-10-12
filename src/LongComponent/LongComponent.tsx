a52961d4cc66772e542bbc4c952d83c9838192f6
Afergon <adrian.afergon@gmail.com>
2019-10-12 17:05:04 +0100

    Fix file styles

--- src/LongComponent/LongComponent.tsx
+++ src/LongComponent/LongComponent.tsx
@@ -1,53 +1,53 @@
 import * as React from 'react';
 import './LongComponent.scss';
 
-type Video = {
-  title: string,
-  category: string,
-  url: string,
-  comentaries: string[]
+interface Video {
+  title: string;
+  category: string;
+  url: string;
+  comments: string[];
 }
 
-type ClasifiedVideos = {
-  [key: string]: Video[]
+interface ClassifiedVideos {
+  [key: string]: Video[];
 }
 
-export const LongComponent: React.FC<{}> = () => { 
-  
+export const LongComponent: React.FC<{}> = () => {
+
   const [categories] = React.useState([] as string []);
   const [videos, setVideos] = React.useState([] as Video[]);
-  const [clasifiedVideos, setClasifiedVideos] = React.useState({} as ClasifiedVideos);
+  const [classifiedVideos, setClassifiedVideos] = React.useState({} as ClassifiedVideos);
   const [selectedVideo, setSelectedVideo] = React.useState({} as Video);
-  const [commentary, setCommentary] = React.useState('');
+  const [newCommentary, setNewCommentary] = React.useState('');
 
   React.useEffect( () => {
     setVideos([
       // ...videos retrieved from api
     ]);
 
-    setClasifiedVideos( 
+    setClassifiedVideos(
       categories.reduce((total, category) => {
         const foundedVideosOnCategory = videos.find((video) => category === video.category);
-        return {...total, [category]: foundedVideosOnCategory}
-      }, {})
+        return {...total, [category]: foundedVideosOnCategory};
+      }, {}),
     );
 
-  }, []);
+  }, [categories, videos]);
 
   const saveCommentary = () => {
     // ... do something
-  }
+  };
 
   return (
   <div className="LongComponent">
     <section>
     <ul>
-      {categories.map((category) => (  
+      {categories.map((category) => (
         <li>
           { category }
           <ul>
-            { clasifiedVideos[category].map( (video) => (
-              <li onClick={() => setSelectedVideo(video)}>{ video.title }</li>)
+            { classifiedVideos[category].map( (video) => (
+              <li onClick={() => setSelectedVideo(video)}>{ video.title }</li>),
             )}
           </ul>
         </li>
@@ -55,16 +55,16 @@ export const LongComponent: React.FC<{}> = () => {
     </ul>
     </section>
     <form>
-      <video></video>
+      <iframe src={selectedVideo.url} title={selectedVideo.title} />
       <ul>
-        {selectedVideo.comentaries.map((commentary) => (
+        {selectedVideo.comments.map((commentary) => (
           <li>{commentary}</li>
         ))}
       </ul>
-      <textarea onChange={(event) => {setCommentary(event.target.value)}}>{commentary}</textarea>
-      <button onClick={saveCommentary}></button>
+      <textarea onChange={(event) => {setNewCommentary(event.target.value); }}>{newCommentary}</textarea>
+      <button onClick={saveCommentary} />
     </form>
   </div>
-)};
+); };
 
 LongComponent.displayName = 'LongComponent';
