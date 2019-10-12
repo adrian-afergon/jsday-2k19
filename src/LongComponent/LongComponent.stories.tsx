ba4207f74a79f03bca786849eec400cda93f4778
Afergon <adrian.afergon@gmail.com>
2019-10-12 17:22:46 +0100

    Remove irrelevant files for the example

--- src/LongComponent/LongComponent.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import {LongComponent} from './LongComponent';
-
-storiesOf('LongComponent', module)
-  .add('with text', () => (
-  <LongComponent />
-));
