c402af914f6392772a463abbca920f0082e65e06
Afergon <adrian.afergon@gmail.com>
2019-10-13 16:44:02 +0100

    Create videos navbar for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { VideosNavbar } from './containers/VideosNavbar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        My videos
       </header>
+      <section>
+        <VideosNavbar />
+      </section>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/containers/VideosNavbar/VideosNavbar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './VideosNavbar.scss';
+
+export const VideosNavbar: React.FC<{}> = () => (
+  <nav className="VideosNavbar">
+    Hello from VideosNavbar!
+  </nav>
+);
+
+VideosNavbar.displayName = 'VideosNavbar';
