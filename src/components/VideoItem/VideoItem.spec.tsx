ace1a90b84c0139af5aa10196833d08e9f08c084
Afergon <adrian.afergon@gmail.com>
2019-10-19 20:10:10 +0100

    Refactor tests for video item

--- src/components/VideoItem/VideoItem.spec.tsx
+++ src/components/VideoItem/VideoItem.spec.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, RenderResult } from '@testing-library/react';
 import { VideoItem} from './';
 
 describe('VideoItem', () => {
-  it('should display the default message', () => {
-    const selectAVideo = jest.fn();
-    const aVideoName = 'irrelevant video name';
-    const { getByText, queryByText } = render(
+  let selectAVideo: () => void;
+  let aVideoName: string;
+  let videoItem: RenderResult;
+
+  beforeEach(() => {
+    selectAVideo = jest.fn();
+    aVideoName = 'irrelevant video name';
+    videoItem = render(
       <VideoItem onClick={selectAVideo}>{aVideoName}</VideoItem>,
     );
-    expect(queryByText(aVideoName)).toBeTruthy();
+  });
+
+  it('should display the default message', () => {
+    expect(videoItem.queryByText(aVideoName)).toBeTruthy();
   });
 
   it('should do something when is clicked', () => {
-    const selectAVideo = jest.fn();
-    const aVideoName = 'irrelevant video name';
-    const { getByText, queryByText } = render(
-      <VideoItem onClick={selectAVideo}>{aVideoName}</VideoItem>,
-    );
-    fireEvent.click(getByText(aVideoName));
+    fireEvent.click(videoItem.getByText(aVideoName));
     expect(selectAVideo).toHaveBeenCalled();
   });
 });
