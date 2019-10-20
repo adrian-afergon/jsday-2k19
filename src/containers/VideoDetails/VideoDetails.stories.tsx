03d38559e1b4b81ed86248eb379a23915f744ba7
Afergon <adrian.afergon@gmail.com>
2019-10-20 20:48:49 +0100

    Fix story

--- src/containers/VideoDetails/VideoDetails.stories.tsx
+++ src/containers/VideoDetails/VideoDetails.stories.tsx
@@ -1,8 +1,9 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import {VideoDetails} from './VideoDetails';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('VideoDetails', module)
   .add('with text', () => (
-  <VideoDetails selectedVideo={null}/>
+  <VideoDetails selectedVideo={null} onSave={action('Clicked')}/>
 ));
