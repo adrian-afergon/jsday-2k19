a2b006e11f40d956007c81afe4f3c4e2dd0195f4
Afergon <adrian.afergon@gmail.com>
2019-10-22 21:12:11 +0100

    Update Videos

--- src/containers/Videos/Videos.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import {Videos} from './Videos';
-
-storiesOf('Videos', module)
-  .add('with text', () => (
-  <Videos />
-));
